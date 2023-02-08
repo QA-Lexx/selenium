52cbc708848509dca43619a0f9d752dbe5064599
QA-Lexx <ilin_001@mail.ru>
2023-02-09 00:34:02 +0300

    delete chromedriver and change tests

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -19,7 +19,6 @@ class CallbackTest {
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-        //System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
 
     @BeforeEach
@@ -30,7 +29,6 @@ class CallbackTest {
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
-        //driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -41,21 +39,17 @@ class CallbackTest {
 
     @Test
     void shouldTestV1() {
-        driver.get("http://localhost:9999");
-        List<WebElement> inputs = driver.findElements(By.tagName("input"));
-        inputs.get(0).sendKeys("Иванов Иван");
-        inputs.get(1).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79210987654");
         driver.findElement(By.className("checkbox__text")).click();
         driver.findElement(By.tagName("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
-        //assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.trim());
     }
 
     @Test
     void shouldTestV2() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
@@ -63,8 +57,5 @@ class CallbackTest {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
-        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
-        //driver.findElement(By.cssSelector("[data-test-id=order-success]")).click();
-        //assertEquals("Ваша заявка успешно отправлена!", text.trim());
     }
 }
